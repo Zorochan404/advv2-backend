d8832e2967b3611822ba70b0266fc091ff3a3732
Zorochan404 <dram8898@gmail.com>
2026-01-05 17:32:24 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ app.use(
 app.use(
   cors({
     origin: process.env.ALLOWED_ORIGINS?.split(",") || [
-      "http://localhost:3000", "https://advv2-admin.vercel.app/"
+      "http://localhost:3000", "https://advv2-admin.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
